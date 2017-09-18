246aecca3dc4f4466a3090fecc696b7ff9f75286
yangbingdong <yangbingdong1994@gmial.com>
2017-09-18 23:37:25 +0800

    add hexo image with other user

---
+++ without-root/docker-entrypoint.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+set -e
+
+GIT_USER_NAME=${GIT_USER_NAME:-"yangbingdong"}
+
+GIT_USER_MAIL=${GIT_USER_MAIL:-"yangbingdong1994@gmail.com"}
+
+git config --global user.name $GIT_USER_NAME
+
+git config --global user.email $GIT_USER_MAIL
+
+exec "$@"
