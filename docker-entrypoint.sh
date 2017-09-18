0032659d740cf10ca4e25a74aa684a4d1cabff3f
masteranthoneyd <masteranthoeyd@163.com>
2017-09-18 16:16:13 +0800

    update entrypoint

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
-set -e
 
 GIT_USER_NAME=${GIT_USER_NAME:-"yangbingdong"}
 
@@ -8,3 +7,5 @@ GIT_USER_MAIL=${GIT_USER_MAIL:-"yangbingdong1994@gmail.com"}
 git config --global user.name $GIT_USER_NAME
 
 git config --global user.email $GIT_USER_MAIL
+
+exec "$@"
