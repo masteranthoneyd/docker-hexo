1cda1da37e1c542b0cc805a12efcd83694d6b4ce
masteranthoneyd <masteranthoeyd@163.com>
2017-09-21 10:21:54 +0800

    update entrypoint

--- without-root/docker-entrypoint.sh
+++ without-root/docker-entrypoint.sh
@@ -1,7 +1,12 @@
 #!/bin/sh
 set -e
 
-FLAG=${FLAG:-execute}
+grep docker-entrypoint.sh | grep -v grep >/dev/null
+if [ $? -eq 0 ]; then
+    exit 1
+fi
+
+# export FLAG=${FLAG:-execute}
 
 GIT_USER_NAME=${GIT_USER_NAME:-yangbingdong}
 
@@ -9,8 +14,8 @@ GIT_USER_MAIL=${GIT_USER_MAIL:-yangbingdong1994@gmail.com}
 
 NEW_USER_NAME=${NEW_USER_NAME:-ybd}
 
-if [ "$FLAG"x = executex ]
-then
+# if [ "$FLAG"x = executex ]
+# then
 
 /usr/sbin/usermod -l ${NEW_USER_NAME} ${USER_NAME}
 
@@ -26,9 +31,9 @@ git config --system user.name $GIT_USER_NAME
 
 git config --system user.email $GIT_USER_MAIL
 
-FLAG=invalid
+# export FLAG=invalid
 
 su ${NEW_USER_NAME}
 
-fi
+# fi
 
